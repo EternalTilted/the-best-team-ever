e4c7d0a3d18c7211f121b1c554b4071e7f74353a
andreyka-konareyka <andrey.andrey.ischenko@mail.ru>
2024-02-25 23:03:03 +0300

    Изменение названия тестовой функции.

--- untitled/src/IsPrimeTest.java
+++ untitled/src/IsPrimeTest.java
@@ -4,54 +4,54 @@ class IsPrimeTest {
 
     @org.junit.jupiter.api.Test
     void isPrime1() {
-        assertEquals(IsPrime.isPrime(1), true);
+        assertEquals(IsPrime.IsPrime(1), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime2() {
-        assertEquals(IsPrime.isPrime(2), true);
+        assertEquals(IsPrime.IsPrime(2), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime3() {
-        assertEquals(IsPrime.isPrime(3), true);
+        assertEquals(IsPrime.IsPrime(3), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime4() {
-        assertEquals(IsPrime.isPrime(4), false);
+        assertEquals(IsPrime.IsPrime(4), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime5() {
-        assertEquals(IsPrime.isPrime(5), true);
+        assertEquals(IsPrime.IsPrime(5), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime6() {
-        assertEquals(IsPrime.isPrime(6), false);
+        assertEquals(IsPrime.IsPrime(6), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime7() {
-        assertEquals(IsPrime.isPrime(7), true);
+        assertEquals(IsPrime.IsPrime(7), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime8() {
-        assertEquals(IsPrime.isPrime(8), false);
+        assertEquals(IsPrime.IsPrime(8), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime9() {
-        assertEquals(IsPrime.isPrime(9), false);
+        assertEquals(IsPrime.IsPrime(9), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime10() {
-        assertEquals(IsPrime.isPrime(10), false);
+        assertEquals(IsPrime.IsPrime(10), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime11() {
-        assertEquals(IsPrime.isPrime(11), true);
+        assertEquals(IsPrime.IsPrime(11), true);
     }
     @org.junit.jupiter.api.Test
     void isPrime12() {
-        assertEquals(IsPrime.isPrime(12), false);
+        assertEquals(IsPrime.IsPrime(12), false);
     }
     @org.junit.jupiter.api.Test
     void isPrime13() {
-        assertEquals(IsPrime.isPrime(13), true);
+        assertEquals(IsPrime.IsPrime(13), true);
     }
 }
