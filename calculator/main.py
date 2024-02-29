4b65a8586b786900b3e325285ce00e3a0ff92e4d
andreyka-konareyka <andrey.andrey.ischenko@mail.ru>
2024-02-29 17:52:33 +0300

    Реализация добавления кнопок и функционала для операций: плюс, минус, умножить, разделить, скобки, удалить все символы, удалить один символ, вычислить выражение, процент.

--- calculator/main.py
+++ calculator/main.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 
 import sys
 
@@ -23,17 +23,107 @@ class Window(QMainWindow):
         self.button_9 = create_button_9()
         self.button_0 = create_button_0()
 
-        self.plus = create_plus()
-        self.minus = create_minus()
-        self.mult = create_mult()
-        self.divide = create_divide()
-        self.open_braket = create_open_braket()
-        self.close_braket = create_close_braket()
-        self.erase = create_erase()    # удалить все символы
-        self.delete = create_delete()  # удалить один символ
-        self.equal = create_equal()
-        self.percent = create_percent()
+        self.plus = self.create_plus()
+        self.minus = self.create_minus()
+        self.mult = self.create_mult()
+        self.divide = self.create_divide()
+        self.open_bracket = self.create_open_bracket()
+        self.close_bracket = self.create_close_bracket()
+        self.erase = self.create_erase()    # удалить все символы
+        self.delete = self.create_delete()  # удалить один символ
+        self.equal = self.create_equal()
+        self.percent = self.create_percent()
 
+    def on_plus_clicked_slot(self):
+        self.label.setText(self.label.text() + '+')
+
+    def create_plus(self):
+        tmp = QPushButton('+', self)
+        tmp.clicked.connect(self.on_plus_clicked_slot)
+        return tmp
+
+    def on_minus_clicked_slot(self):
+        self.label.setText(self.label.text() + '-')
+
+    def create_minus(self):
+        tmp = QPushButton('-', self)
+        tmp.clicked.connect(self.on_minus_clicked_slot)
+        return tmp
+
+    def on_mult_clicked_slot(self):
+        self.label.setText(self.label.text() + '*')
+
+    def create_mult(self):
+        tmp = QPushButton('*', self)
+        tmp.clicked.connect(self.on_mult_clicked_slot)
+        return tmp
+
+    def on_divide_clicked_slot(self):
+        self.label.setText(self.label.text() + '/')
+
+    def create_divide(self):
+        tmp = QPushButton('/', self)
+        tmp.clicked.connect(self.on_divide_clicked_slot)
+        return tmp
+
+    def on_open_bracket_clicked_slot(self):
+        self.label.setText(self.label.text() + '(')
+
+    def create_open_bracket(self):
+        tmp = QPushButton('(', self)
+        tmp.clicked.connect(self.on_open_bracket_clicked_slot)
+        return tmp
+
+    def on_close_bracket_clicked_slot(self):
+        self.label.setText(self.label.text() + ')')
+
+    def create_close_bracket(self):
+        tmp = QPushButton(')', self)
+        tmp.clicked.connect(self.on_close_bracket_clicked_slot)
+        return tmp
+
+    def on_erase_clicked_slot(self):
+        self.label.setText('')
+
+    def create_erase(self):
+        tmp = QPushButton('C', self)
+        tmp.clicked.connect(self.on_erase_clicked_slot)
+        return tmp
+
+    def on_delete_clicked_slot(self):
+        self.label.setText(self.label.text()[:-1])
+
+    def create_delete(self):
+        tmp = QPushButton('CE', self)
+        tmp.clicked.connect(self.on_delete_clicked_slot)
+        return tmp
+
+    def on_equal_clicked_slot(self):
+        if self.label.text() == '':
+            self.label.setText('0')
+            return
+
+        if self.label.text() == 'SyntaxError':
+            return
+
+        try:
+            tmp = self.label.text().replace('%', '/100')
+            self.label.setText(str(eval(tmp)))
+        except SyntaxError:
+            self.label.setText('SyntaxError')
+
+    def create_equal(self):
+        tmp = QPushButton('=', self)
+        tmp.clicked.connect(self.on_equal_clicked_slot)
+        return tmp
+
+    def on_percent_clicked_slot(self):
+        self.label.setText(self.label.text() + '%')
+
+    def create_percent(self):
+        tmp = QPushButton('%', self)
+        tmp.clicked.connect(self.on_percent_clicked_slot)
+        return tmp
 
 
 
